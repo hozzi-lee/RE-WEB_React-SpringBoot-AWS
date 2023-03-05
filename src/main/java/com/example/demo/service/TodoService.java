7676cede07d3390b3b41f719050b3346d30a0678
hozzi-lee <hogenie93@gmail.com>
2023-03-05 23:20:32 +0900

    WEB_BACK) Update Todo

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -68,6 +69,36 @@ public class TodoService {
 	public List<TodoEntity> retrieve(final String userId) {
 		log.debug("$$$$$ TodoService.retrieve in userId::: {}", userId);
 		return repository.findByUserId(userId);
+	}
+	
+	public List<TodoEntity> update(final TodoEntity entity) {
+		
+		//(1) 저장할 엔티티가 유효한지 확인.
+		validate(entity);
+		
+		//(2) 넘겨받은 엔티티 id를 이용해 TodoEntity를 가져온다. 존재하지 않는 엔티티는 업데이트 불가능.
+		final Optional<TodoEntity> original = repository.findById(entity.getId());
+		/* 일반
+		if(original.isPresent()) {
+			final TodoEntity todo = original.get();
+			todo.setTitle(entity.getTitle());
+			todo.setDone(entity.isDone());
+			
+			repository.save(todo);
+		}
+		*/
+		//Lambda
+		original.ifPresent(todo -> {
+			//(3) 반환된 TodoEntity가 존재하면 값을 새 Entity 값으로 덮어 씌움.
+			todo.setTitle(entity.getTitle());
+			todo.setDone(entity.isDone());
+			
+			//(4) 데이터베이스에 새 값을 저장.
+			repository.save(todo);
+		});
+		
+		// Retrieve Todo에서 만든 메서드를 이용해 사용자의 모든 Todo 리스트를 리턴
+		return retrieve(entity.getUserId());
 	}
 	
 	// 리팩토링한 메서드
